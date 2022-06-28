db6afe039714dc373755bbfc60b5ab7151301193
EC2 Default User <ec2-user@ip-172-31-9-2.ec2.internal>
2022-06-28 15:44:34 +0000

    second commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,21 +7,31 @@ class ListsController < ApplicationController
   def create
    list = List.new(list_params)
    list.save
-   redirect_to '/top'
+   # redirect_to '/top' を削除して、以下コードに変更
+   # 詳細画面へリダイレクト
+   redirect_to list_path(list.id)
   end
 
   def index
-  　@lists = List.all
+   @lists = List.all
   end
   
   def show
+   @list = List.find(params[:id])
   end
   
   def edit
+   @list = List.find(params[:id])
+  end
+  
+  def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)
   end
   
   private
   def list_params
-   params.require(:list).permit(:title, :body)
+   params.require(:list).permit(:title, :body, :image)
   end
 end
